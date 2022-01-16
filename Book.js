2b22a02f9ee8d95fdb943090987ae0ead46eab8c
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2022-01-17 00:30:54 +0500

    task 1-2

---
+++ Book.js
@@ -0,0 +1,32 @@
+let count = 0;
+
+class Book {
+  constructor({
+    title,
+    description,
+    authors,
+    favorite,
+    fileCover,
+    fileName,
+  } = {}) {
+    this.id = String(count++);
+    this.title = title || "Book " + this.id;
+    this.description = description || "";
+    this.authors = authors || "";
+    this.favorite = favorite || "";
+    this.fileCover = fileCover || "";
+    this.fileName = fileName || "";
+  }
+
+  update({ title, description, authors, favorite, fileCover, fileName }) {
+    this.title = title !== undefined ? title : this.title;
+    this.description =
+      description !== undefined ? description : this.description;
+    this.authors = authors !== undefined ? authors : this.authors;
+    this.favorite = favorite !== undefined ? favorite : this.favorite;
+    this.fileCover = fileCover !== undefined ? fileCover : this.fileCover;
+    this.fileName = fileName !== undefined ? fileName : this.fileName;
+  }
+}
+
+module.exports = Book;
